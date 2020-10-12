94e493d6aeeb3d66cf5bbcb8a464917ffe452070
mikey4code <mikeg4honor@gmail.com>
2020-10-12 19:43:58 -0400

    initialize commit

--- client/src/components/Transaction.vue
+++ client/src/components/Transaction.vue
@@ -1,13 +1,28 @@
 <template>
    <v-layout column>
     <v-flex xs6 offset-xs3>
-      <panel title="Register">
+      <panel title="Transaction">
         <form name="tab-tracker-form"
               autocomplete="off">
-          <v-text-field label="Email"
+          <v-text-field label="Transaction type"
                         v-model="email"></v-text-field>
           <br>
-          <v-text-field label="Password"
+          <v-text-field label="First Name"
+                        type="password"
+                        v-model="password"
+                        autocomplete="new-password"></v-text-field>
+          <br>
+          <v-text-field label="Last Name"
+                        type="password"
+                        v-model="password"
+                        autocomplete="new-password"></v-text-field>
+                        <br>
+          <v-text-field label="Phone Number"
+                        type="password"
+                        v-model="password"
+                        autocomplete="new-password"></v-text-field>
+                        <br>
+          <v-text-field label="Amount"
                         type="password"
                         v-model="password"
                         autocomplete="new-password"></v-text-field>
@@ -18,7 +33,7 @@
         <v-btn dark
                class="cyan"
                @click="register">
-          Register
+          Submit
         </v-btn>
       </panel>
     </v-flex>
