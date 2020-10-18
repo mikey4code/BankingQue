82eddbb7db8ac753c7f7e791026f7baf9a3abb87
sandy13c <adilfanta@gmail.com>
2020-10-18 16:19:28 -0400

    Update routes.js

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,12 @@
 
-const AuthenticationController = require('./controllers/AuthenticationController')
-const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+const AuthenticationController = require('./controllers/AuthenticationController');
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy');
 //const BalanceSummaryController = require('./controllers/BalanceSummaryController')
-const NewAccountController = require('./controllers/NewAccountController')
-const TransactionController = require('./controllers/TransactionController')
+const NewAccountController = require('./controllers/NewAccountController');
+const TransactionController = require('./controllers/TransactionController');
+const NewTransactionController = require('./controllers/NewTransactionController');
+const DebitController = require('./controllers/DebitController');
+const CreditController = require('./controllers/CreditController');
 
 //const isAuthenticated = require('./policies/isAuthenticated')
 
@@ -11,17 +14,25 @@ const TransactionController = require('./controllers/TransactionController')
 module.exports = (app) => {
     app.post('/register',
         AuthenticationControllerPolicy.register,
-        AuthenticationController.register)
+        AuthenticationController.register);
     
         app.post('/login',
-            AuthenticationController.login)
+            AuthenticationController.login);
         
         app.post('/newaccount',
-            NewAccountController.post)
+            NewAccountController.post);
 
         app.get('/viewtrans',
-            TransactionController.index)
+            TransactionController.index);
 
         app.post('/viewtrans',
-            TransactionController.post)
+            TransactionController.post);
+        app.post('/addtrans',
+            NewTransactionController.post);
+        app.post('/addebit',
+            DebitController.post);
+            
+        app.post('/addcredit',
+            CreditController.post);
+        
 }
