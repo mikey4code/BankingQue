4fc6d7bb49e33a5504491953bac97213911f1615
mikey4code <mikeg4honor@gmail.com>
2020-11-09 22:55:34 -0500

    Update BankReport.vue

--- client/src/views/dashboard/tables/BankReport.vue
+++ client/src/views/dashboard/tables/BankReport.vue
@@ -96,3 +96,61 @@
     </base-material-card>
   </v-container>
 </template>
+
+<script>
+import {mapState} from 'vuex'
+import TransacHisService from '@/services/TransacHisService'
+export default {
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Transaction Type',
+          value: 'trantype'
+        },
+        {
+          text: 'First Name',
+          value: 'firstn'
+        },
+        {
+          text: 'Last Name',
+          value: 'lastn'
+        },
+        {
+          text: 'Account Number',
+          value: 'accnumber'
+        },
+        {
+          text: 'Address',
+          value: 'address'
+        },
+        {
+          text: 'Phone #',
+          value: 'phone'
+        },
+        {
+          text: 'DOB',
+          value: 'dob'
+        }
+      ],
+      transactions: []
+    }
+  },
+  computed: {
+    ...mapState([
+      'isUserLoggedIn',
+      'user'
+    ])
+  },
+  async mounted () {
+    if (this.isUserLoggedIn) {
+      this.transactions = (await TransacHisService.index()).data
+      console.log('first account ', this.transactions)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
